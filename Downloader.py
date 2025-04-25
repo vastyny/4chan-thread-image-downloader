a41d8709bbf7bf3a80bf461c766fca15c452f0bd
vady valle <vastyny@gmail.com>
2025-04-25 15:44:57 -0300

    Add files via upload

--- Downloader.py
+++ Downloader.py
@@ -1,9 +1,11 @@
 import os
 import requests
+import cloudscraper
 from tkinter import filedialog
 from bs4 import BeautifulSoup
 
-#pick the link and the name of 
+scraper = cloudscraper.create_scraper()
+#pick the link of the thread and the name of the foder that's gonna be made
 thread_url = input('put the link of the thread')
 if not thread_url:
     exit()
@@ -11,7 +13,8 @@ output_folder= input('name of the folder that u gonna make')
 if not output_folder:
     output_folder='thread'
 var = 0
-#ask for the directory
+
+#ask for the directory to create afolder
 while var == 0:
     directory = filedialog.askdirectory(title='Select a directory')
     if directory:
@@ -25,6 +28,7 @@ while var == 0:
             continue
     else:
         print('select a directory')
+
 #pick the diretory and open
 new_path = os.path.join(directory, output_folder)
 try:
@@ -38,7 +42,7 @@ except Exception as e:
 
 #get the link and the html
 try:
-    response = requests.get(thread_url)
+    response = scraper.get(thread_url)
 except requests.exceptions.ConnectionError as E:
     print(f'print error, non valid link')
     exit()
@@ -48,12 +52,14 @@ except Exception as d:
 if response.status_code != 200:
     print(f'Failed to get the link')
     exit()
-#image getter, im not good at bs4
+#image getter, create a bs4 object
 soup = BeautifulSoup(response.text, 'html.parser')
+#find all ocurrence of tag, a hlink(hyperlink), to download after, for every link
 images = soup.find_all('a', class_='fileThumb')
 if not images:
     print('not images found')
 for image in images:
+    #yeah
     image_url = 'https:' + image['href']
     image_name = os.path.basename(image_url)
     image_path = os.path.join(new_path, image_name)
@@ -61,7 +67,7 @@ for image in images:
     image_response = requests.get(image_url)
     if image_response.status_code == 200:
         with open(image_path, 'wb') as file:
-            file.write(image_response.content)
+            file.write(image_response.content)#SINCE content gets bytes, and wb is write in bytes, yeah, and i need to put png or something like that before
             print(f'downloaded in {image_path}')
     else:
         print(f'failed to download {image_url}, status_code = {image_response.status_code}')
